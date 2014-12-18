0172442d1f18d341cbed195b4a7e97853a510b53
Harley <hdavis0090@gmail.com>
2014-12-17 19:40:07 -0800

    Add script for chart processing.

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+	var key = '123' //todo figure out a way to get this from ENV
+	var summonerName = 'harley'
+	var id
+
+	//call to get summoner ID
+	$.ajax({
+	  url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summonerName + '?api_key=' + key,
+	  success: function(data, status) {
+	  id = data[summonerName].id
+	  
+	  	//call to get match history
+	  	 $.ajax({
+			  url: 'https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/' + id + '?api_key=' + key,
+			  success: function(data, status) {
+				test = data.matches[0].queueType
+				$( "span" ).text(id).show();
+			  }
+			});
+	  }
+	});
+	
+	
+	$( "form" ).submit(function( event ) {
+
+	  if ( $( "input:first" ).val() != null ) {
+		//
+		
+			var num = $( "input:first" ).val()
+			var myChart=
+				{
+				"graphset":[
+					{
+						"type":"bar",
+						"title":{
+							"text":"Bar"
+						},
+						"series":[
+							{
+								"values":[11,26,7,44,11,28,42,26,13,10]
+							},
+						]
+					}
+				]
+				}
+				zingchart.render({
+			id:"myChartDiv",
+			data:myChart,
+			height:400,
+			width:"100%"
+		});
+		return;
+	  }
+	 
+	  $( "span" ).text( "Not valid!" ).show().fadeOut( 1000 );
+	  event.preventDefault();
+	});
+  });
