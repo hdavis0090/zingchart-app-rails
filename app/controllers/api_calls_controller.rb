d682ff58be14ef8a0c7dbee6c207386c7baa35ea
Harley <hdavis0090@gmail.com>
2014-12-21 18:26:59 -0800

    Created apiCall private helper method for controller

--- app/controllers/api_calls_controller.rb
+++ app/controllers/api_calls_controller.rb
@@ -3,28 +3,23 @@ class ApiCallsController < ApplicationController
 	def getSummonerId
 		summonerName = params['summonerName']
 		url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/#{summonerName}?api_key=#{ENV['KEY']}"
-
-        begin
-		  response = HTTParty.get(url)
-		  render json: response.body, status: response.code
-        rescue Exception
-          render json: "Internal server error", status: 500
-        end
+		apiCall(url)
 	end
 	
 	def getMatchHistory
 		summonerId = params['summonerId']
 		url = "https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/#{summonerId}/recent?api_key=#{ENV['KEY']}"
-		begin
-		  response = HTTParty.get(url)
-		  render json: response.body, status: response.code
-		rescue Exception
-		  render json: "Internal server error", status: 500
-		end	
+		apiCall(url)
 	end
 	
 	def getChallengers
 		url = "https://na.api.pvp.net/api/lol/na/v2.5/league/challenger?type=RANKED_SOLO_5x5&api_key=#{ENV['KEY']}"
+		apiCall(url)
+	end
+	
+	private
+	
+	def apiCall(url)
 		begin
 		  response = HTTParty.get(url)
 		  render json: response.body, status: response.code
